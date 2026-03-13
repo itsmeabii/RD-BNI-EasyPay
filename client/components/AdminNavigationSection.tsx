36f662089b758006c0a662d2f0055c38ab6df56a
Azylle713 <gamazonellyza@gmail.com>
2026-03-13 14:42:50 +0800

    Deleted AdminNavigationSection.tsx

--- client/components/AdminNavigationSection.tsx
+++
@@ -1,6 +0,0 @@
-import { UserNavigationSection } from "@/components/UserNavigationSection";
-import { ADMIN_NAV_ITEMS } from "@/constants/routes";
-
-export const AdminNavigationSection = (): JSX.Element => {
-  return <UserNavigationSection menu_items={ADMIN_NAV_ITEMS} />;
-};
